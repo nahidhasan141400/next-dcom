e84b1427f1e5bf0784a747f11e717c9ac53ab76c
nahidhasan141400 <nahidhasan141400@gmail.com>
2023-02-01 19:38:26 +0600

    add full home page

--- components/nav/Navbar.jsx
+++ components/nav/Navbar.jsx
@@ -62,7 +62,7 @@ export default function Navbar() {
                 <div className="my-4 md:hidden">
                     <div className="relative">
                         <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                        <MagnifyingGlassIcon  className="h-6 h-6 text-gray-500" />
+                        <MagnifyingGlassIcon  className="h-6 text-gray-500" />
                         </span>
 
                         <input type="text" className="w-full py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300" placeholder="Search"/>
